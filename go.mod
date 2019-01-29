aec5a6f7a257f039f37ad2eb3edc5f636aa3275b
Isaac Diamond <isaacd9@gmail.com>
2019-01-28 23:31:17 -0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/isaacd9/picoblog
+
+require (
+	github.com/russross/blackfriday/v2 v2.0.1
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.3
+)
---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/russross/blackfriday/v2"
+	flag "github.com/spf13/pflag"
+)
+
+const VERSION = "0.1"
+
+var (
+	title = flag.String("title", "Picoblog", "Title for blog")
+	web   = flag.Bool("web", false, "Display feed in browser")
+
+	postTemplate = template.Must(template.New("post").Parse(
+		`
+<h1 id="{{ .Title }}">{{ .Title }}</h1>
+<b>{{ .Timestamp.Format "January 2nd, 2006" }}</b>
+{{ .Contents }}
+`))
+
+	blogTemplate = template.Must(template.New("blog").Parse(
+		`
+<!DOCTYPE html>
+<head>
+<title>{{ .Title }}</title>
+<style>
+body {
+	margin: 0 auto;
+	padding: 2em 0px;
+	max-width: 800px;
+	color: #888;
+	font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+	font-size: 14px;
+	line-height: 1.4em;
+}
+h1,h2,h3,h4   {color: #000;}
+a {color: #000;}
+a:visited {color: #888;}
+</style>
+</head>
+<body>
+<h4 style="padding-bottom: 2em">{{ .Title }}</h4>
+`))
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage:
+  picoblog takes a file of post paths
+
+  Examples:
+	picoblog first.md second.md
+	picoblog post_directory --web
+	picoblog pist_directory other_post_directory --html
+
+  Flags:
+`)
+		flag.PrintDefaults()
+	}
+
+	flag.ErrHelp = fmt.Errorf("")
+}
+
+func main() {
+	flag.Parse()
+
+	postNames := flag.Args()
+	if len(postNames) == 0 {
+		fmt.Fprintf(os.Stderr, "ERROR: No post provided\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if postNames[0] == "version" {
+		fmt.Fprintf(os.Stderr, "%s\n", VERSION)
+		return
+	}
+
+	posts := getPosts(postNames)
+
+	renderHtml(bufio.NewWriter(os.Stdout), posts)
+}
+
+type post struct {
+	Title     string
+	Timestamp time.Time
+	Contents  string
+}
+
+func (p *post) Render(w io.Writer) {
+	htmlContents := blackfriday.Run([]byte(p.Contents))
+	postTemplate.Execute(w, post{
+		p.Title,
+		p.Timestamp,
+		string(htmlContents),
+	})
+}
+
+func getPosts(filenames []string) (posts []*post) {
+	for _, filename := range filenames {
+		post, err := getPost(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error building post: %v", err)
+		}
+
+		posts = append(posts, post)
+	}
+	return posts
+}
+
+func getPost(filename string) (*post, error) {
+	info, err := os.Stat(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to stat file %q: %v", filename, err)
+	}
+
+	p := &post{
+		Title:     info.Name(),
+		Timestamp: info.ModTime(),
+	}
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file %q: %v", filename, err)
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(bufio.NewReader(f))
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file %q: %v", filename, err)
+	}
+
+	p.Contents = string(b)
+	return p, nil
+}
+
+func renderHtml(w io.Writer, posts []*post) {
+	for _, p := range posts {
+		p.Render(w)
+	}
+}
